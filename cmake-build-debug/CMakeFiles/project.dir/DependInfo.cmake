6fbef5224d99e0de68c5b9f006099ac5ca6b7942
Marwan Mohamed Abd Elmonem <88461870+Marwan513@users.noreply.github.com>
2022-05-31 23:02:19 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/User.cpp" "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/cmake-build-debug/CMakeFiles/project.dir/User.cpp.obj"
+  "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/friends.cpp" "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/cmake-build-debug/CMakeFiles/project.dir/friends.cpp.obj"
+  "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/main.cpp" "C:/Users/Marwan/Desktop/Project_20190513_20160122_20160250_20190648_20190421/cmake-build-debug/CMakeFiles/project.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
